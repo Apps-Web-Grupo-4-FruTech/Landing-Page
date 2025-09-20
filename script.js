879b8142692aff3f1c69c4b2c60399bb45ad6bff
Samuel <samuelbonifacio015@gmail.com>
2025-09-20 14:09:27 -0500

    feat: added features & purchase sections

--- script.js
+++ script.js
@@ -3,4 +3,18 @@
  * Creacion de boton hamburguesa
  */
 
-const btn = document.querySelector();
+const btn = document.querySelector(".menu-toggle");
+const menu = document.querySelector("nav-menu");
+
+//EventListener para click
+btn.addEventListener("click", () => {
+    //Al dar click en el icono de menu hamburguesa, se alterna
+    menu.classList.toggle("active");
+})
+
+//Se cierra el menu horizontal al hacer otro click
+document.querySelectorAll(".nav-link").forEach(link => {
+    link.addEventListener("click", () => {
+        menu.classList.remove("active");
+    })
+})
